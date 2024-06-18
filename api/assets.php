e29f5136e2ee2f22e4281bf81db6bc2c08f16319
Ahmad Santoso <ahmadsantoso800@gmail.com>
2024-06-19 01:42:41 +0700

    testing vercel

--- api/assets.php
+++ api/assets.php
@@ -1,7 +1,19 @@
 <?php
 // File: api/assets.php
+
 $queries = array();
 parse_str($_SERVER['QUERY_STRING'], $queries);
-$assets = $queries['assets'];
+$file = $queries['file'];
 
-echo require __DIR__ . '/../' . $assets;
+// Pastikan file path adalah path yang benar
+$file_path = __DIR__ . '/../' . $file;
+
+if (file_exists($file_path)) {
+    $mime_type = mime_content_type($file_path);
+    header('Content-Type: ' . $mime_type);
+    readfile($file_path);
+} else {
+    http_response_code(404);
+    echo "File not found.";
+}
+?>
