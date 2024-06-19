feae38bfc82cf4bee1079621783e4f82998ecc95
Ahmad Santoso <ahmadsantoso800@gmail.com>
2024-06-20 05:28:11 +0700

    testing vercel

--- config/database.php
+++ config/database.php
@@ -81,12 +81,12 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'laravel'),
-            'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'url' => env('postgresql://destinova_explore_owner:47jXifDLZeCT@ep-falling-bonus-a1z9y999.ap-southeast-1.aws.neon.tech/destinova_explore?sslmode=require'),
+            // 'host' => env('DB_HOST', '127.0.0.1'),
+            // 'port' => env('DB_PORT', '5432'),
+            // 'database' => env('DB_DATABASE', 'laravel'),
+            // 'username' => env('DB_USERNAME', 'root'),
+            // 'password' => env('DB_PASSWORD', ''),
             'charset' => env('DB_CHARSET', 'utf8'),
             'prefix' => '',
             'prefix_indexes' => true,
